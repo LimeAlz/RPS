18f03905c07c2a8c9533f25a39205bdcf780e055
LimeAlz <limealz750@gmail.com>
2023-03-06 14:30:41 +1030

    added styling

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let counter = 0;
+let w = 0;
+let l =0;
+let d=0;
+
 function getComputerChoice(){
     let test = ["Rock","Paper","Scissor"];
     let num = Math.floor(Math.random()*3) ;
@@ -7,66 +12,150 @@ function getComputerChoice(){
     return result;
 }
 
-function playRound(cc){
-    let pc=prompt("Enter your choice of Rock,Paper and Scissor","");
+function playRound(cc,pc){
+    // let pc=prompt("Enter your choice of Rock,Paper and Scissor","");
     let b;
     if(cc=="Rock" && pc =="Paper"){
-        console.log("You win");
+        console.log("You win"+" "+cc+" "+pc);
          b = "w";
+         if(counter<5){
+            w++;
+         }
     }
     else if(cc=="Rock" && pc =="Scissor"){
-        console.log("You lose");
+        console.log("You lose"+" "+cc+" "+pc);
         b = "l";
+        if(counter<5){
+            l++;
+        }
 
     }
     else if(cc=="Paper" && pc =="Scissor"){
-        console.log("You win");
+        console.log("You win"+" "+cc+" "+pc);
         b = "w";
+        if(counter <5){
+            w++;
+        }
     }
     else if(cc=="Paper" && pc =="Rock"){
-        console.log("You lose");
+        console.log("You lose"+" "+cc+" "+pc);
         b= "l";
+        if(counter <5){
+            l++;
+        }
     }
     else if(cc=="Scissor" && pc =="Paper"){
-        console.log("You lose");
+        console.log("You lose"+" "+cc+" "+pc);
         b = "l";
+        if(counter <5){
+            l++;
+        }
     }
     else if(cc=="Scissor" && pc =="Rock"){
-        console.log("You win");
+        console.log("You win"+" "+cc+" "+pc);
         b = "w";
+        if(counter <5){
+            w++;
+        }
     }
     else if(cc == pc){
-        console.log("Draw.");
+        console.log("Draw."+" "+cc+" "+pc);
         b="d";
+        if(counter <5){
+            d++;
+        }
     }
     return b;
 }
 
-function game(){
-    let w = 0;
-    let l =0;
-    let r;
-    
-    for(let i=0;i<5;i++){
-        r = playRound(com,pc);
-        if(r == "w"){
-            w = w +1;
+function ouputTimer(time){
+    const timer = document.querySelector('#timer');
+    // timer.style.cssText = "border: solid black 2px;"
+    timer.textContent = `Counter: ${counter}`
+
+}
+
+function reset(){
+    counter = 0;
+    w =0;
+    l=0;
+    d=0;
+}
+
+function game(result,com,pc){
+    const out = document.querySelector("#output")
+    if (counter < 5){
+        if(result == 'w'){
+            out.textContent = `You win, ${pc} beat ${com}`;
         }
-        else if(r ="l"){
-            l = l+1;
+        else if(result == 'l'){
+            out.textContent = `You lose, ${com} beat ${pc}`;
         }
+        else if(result == 'd'){
+            out.textContent = `You draw, both choose ${pc} and ${com}`;
+        }
+        else{
+            out.textContent = "input error";
+        }
+        counter = counter+1;
+        ouputTimer(counter);
     }
-    if(l>w){
-        console.log("You lose");
-    }
-    else if(w>l){
-        console.log("You win");
+    else if (counter === 5){
+
+        out.textContent = "Game Over. 5 rounds have been reached";
+        const result = document.querySelector('#fres');
+        if(w>l){
+            result.textContent =`You win. W:${w} +L${l} +D${d}`;
+        }
+        else if(l>w){
+            result.textContent =`You lose. W:${w} +L${l} +D${d}`;
+        }
+        else{
+            result.textContent =`You draw W:${w} +L${l} +D${d}`;
+        }
+        reset();
+
     }
     else{
-        console.log("Draw+ result "+l+" "+w);
+        return;
     }
+
 }
-let com = getComputerChoice();
-let pc="Paper";
-game();
-// playRound(com,pc);
+
+
+
+
+function chooseRock(){
+    let pc = "Rock";
+    let com = getComputerChoice();
+    let result = playRound(com,pc);
+    game(result,com,pc);
+    
+}
+function choosePaper(){
+    let pc = "Paper";
+    let com = getComputerChoice();
+    let result = playRound(com,pc);
+    game(result,com,pc);
+}
+function chooseScissor(){
+    let pc = "Scissor";
+    let com = getComputerChoice();
+    let result = playRound(com,pc);
+    game(result,com,pc);
+}
+
+
+
+const btnRock = document.querySelector("#rock")
+btnRock.addEventListener('click',chooseRock)
+
+const btnPaper = document.querySelector("#paper")
+btnPaper.addEventListener('click',choosePaper)
+
+const btnScissor = document.querySelector("#scissor")
+btnScissor.addEventListener('click',chooseScissor)    
+
+
+
+
